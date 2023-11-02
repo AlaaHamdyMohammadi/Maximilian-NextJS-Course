49e73af72b9b6a378b8041d16b3b2c8cede99105
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-03 01:29:41 +0200

    Extracting Dynamic Path

--- Section3/CodeSection3/pages/portfolio/[projectId].js
+++ Section3/CodeSection3/pages/portfolio/[projectId].js
@@ -1,4 +1,10 @@
+import {useRouter} from 'next/router';
+
 function ProjectIdPage() {
+  const router = useRouter(); // return object
+  console.log(router.pathname); // return (/portfolio/[projectId]) => path of folder
+  console.log(router.query); // return an object {projectId: 'whatever i written in url'}
+
   return (
     <div>
       <h1>The Project Id Page (Dynamic Page)</h1>
