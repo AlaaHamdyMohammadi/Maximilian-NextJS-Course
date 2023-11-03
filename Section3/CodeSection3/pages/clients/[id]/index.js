c3320fdb43c39387d273c2f1111e50cf8e5d457e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-03 03:11:50 +0200

    Navigate Programmatically

--- Section3/CodeSection3/pages/clients/[id]/index.js
+++ Section3/CodeSection3/pages/clients/[id]/index.js
@@ -1,7 +1,16 @@
+import {useRouter} from 'next/router';
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  function handleNavigate(){
+    //Navigating Programmatically(This way using in submitting form)
+    router.push('/clients/alaa/projectA')
+  }
   return (
     <div>
       <h1>The Client Projects Page</h1>
+      <button onClick={handleNavigate}>Load Project</button>
     </div>
   );
 }
