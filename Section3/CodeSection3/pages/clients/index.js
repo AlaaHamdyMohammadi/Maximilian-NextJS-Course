4e2a971116919370cb07af602d1a737c0e11dc3e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-03 02:54:54 +0200

    Navigate Dynamic Routes

--- Section3/CodeSection3/pages/clients/index.js
+++ Section3/CodeSection3/pages/clients/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 function ClientsPage() {
+  const clients = [
+    {id: 1, name: 'Alaa'},
+    {id: 2, name: 'Hamdy'},
+    {id: 3, name: 'Mohammadi'},
+  ]
   return (
     <div>
       <h1>The clients Page</h1>
+      <ul>
+        {clients.map((client) => <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link> </li>)}
+      </ul>
     </div>
   );
 }
--- Section3/CodeSection3/pages/index.js
+++ Section3/CodeSection3/pages/index.js
@@ -1,6 +1,16 @@
+import Link from 'next/link'
+
 function HomePage(){
     return <div>
         <h1>Home Page</h1>
+        <ul>
+            <li>
+                <Link href='portfolio'>portfolio</Link>
+            </li>
+            <li>
+                <Link href='clients'>client</Link>
+            </li>
+        </ul>
     </div>
 }
 
