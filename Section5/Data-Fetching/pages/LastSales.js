3f0d581724fbb562befaf2c934b18180811aa613
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-07 01:59:12 +0200

    Update

--- Section5/Data-Fetching/pages/LastSales.js
+++ Section5/Data-Fetching/pages/LastSales.js
@@ -1,31 +1,58 @@
 import { useEffect, useState } from "react";
-
+import useSWR from "swr";
 //useSWR(<request-url>, (url) => fetch(url).then(res => res.json()))
 
 function LastSales() {
-    const [sales, setSales] = useState();
-    const [isLoading, setIsLoading] = useState(false);
+  const [sales, setSales] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    `https://nextjs-course-a4784-default-rtdb.firebaseio.com/sales.json`,
+    (url) => fetch(url).then((res) => res.json())
+  );
+
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      `https://nextjs-course-a4784-default-rtdb.firebaseio.com/sales.json`
-    ).then((res) => res.json()).then(data => {
-        // To convert an object to an array
-        const transformedSales = [];
-        for(const key in data){
-            transformedSales.push({id: key, username: data[key].username, volume: data[key].volume})
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-    });
-  }, []);
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if(isLoading) return <h1>isLoading...</h1>
-  if(!sales) return <h1>No data yet...</h1>
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       `https://nextjs-course-a4784-default-rtdb.firebaseio.com/sales.json`
+  //     ).then((res) => res.json()).then(data => {
+  //         // To convert an object to an array
+  //         const transformedSales = [];
+  //         for(const key in data){
+  //             transformedSales.push({id: key, username: data[key].username, volume: data[key].volume})
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //     });
+  //   }, []);
 
-  return <ul>
-    {sales.map((sale) => <li key={sale.id}>{sale.username} - {sale.volume}</li>)}
-  </ul>;
+  if (error) return <h1>No data yet...</h1>;
+  if (!data) return <h1>isLoading Data...</h1>;
+  if (!sales) return <h1>isLoading Sales...</h1>;
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - {sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default LastSales;
