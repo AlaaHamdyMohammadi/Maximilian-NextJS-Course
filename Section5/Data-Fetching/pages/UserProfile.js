8f03cd29d491985503a9f1751be0f5766e281f37
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-06 23:53:40 +0200

    Update

---
+++ Section5/Data-Fetching/pages/UserProfile.js
@@ -0,0 +1,17 @@
+function UserProfile(props) {
+    return (
+        <h1>
+            {props.username}
+        </h1>
+    )
+}
+
+export default UserProfile
+
+export async function getServerSideProps(){
+    return {
+        props:{
+            username: 'Alaa'
+        }
+    }
+}
