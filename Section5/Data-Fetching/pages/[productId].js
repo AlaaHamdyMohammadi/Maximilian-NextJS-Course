8217c46a1e839e2042a09b7ecb5b2a8ccf559a1d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-06 22:06:37 +0200

    Update

--- Section5/Data-Fetching/pages/[productId].js
+++ Section5/Data-Fetching/pages/[productId].js
@@ -2,11 +2,11 @@ import fs from "fs/promises";
 import path from "path";
 
 function ProductDetailsPage(props) {
-    const {product} = props
+  const { product } = props;
   return (
     <div>
       <h1>Title: {product.title}</h1>
-      <h4>Description: {product.dascription}</h4>
+      <h4>Description: {product.description}</h4>
     </div>
   );
 }
@@ -15,19 +15,30 @@ export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.productId;
 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  const jsonData = fs.readFile(filePath);
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
   const product = data.products.find((item) => item.id === productId);
 
-  return{
+  return {
     props: {
-        product,
-        revalidate: 5,
-    }
-  }
+      product,
+      revalidate: 5,
+    },
+  };
+}
 
+export async function getStaticPaths(){
+    //tell Next.js which instance of this dynamic page should be generated
+    return{
+        paths: [
+            {params: {productId: 'p1'}},
+            {params: {productId: 'p2'}},
+            {params: {productId: 'p3'}},
+        ],
+        fallback: false,
+    } 
 }
 
 export default ProductDetailsPage;
