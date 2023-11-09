e7de05e16025acb68915c33fb65369207369003d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-09 04:47:01 +0200

    Prepare Form

--- Section8/API Routes/pages/index.js
+++ Section8/API Routes/pages/index.js
@@ -1,7 +1,33 @@
+import Head from 'next/head';
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
       <h1>The Home Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea rows="5" id="feedback" ref={feedbackInputRef} />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
